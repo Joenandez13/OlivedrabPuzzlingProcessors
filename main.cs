21c1b106f87aed59501d17fac7b032f7d9decc72
Joenandez13 <143644911+Joenandez13@users.noreply.github.com>
2023-09-06 13:28:15 +0000

    PersonalInfo

--- main.cs
+++ main.cs
@@ -1,7 +1,22 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World, Joenathan is here");
+class Program{
+  public static void Main(string[] args) {
+    Console.WriteLine("Enter Name:");
+    string userName = Console.ReadLine();
+
+    Console.WriteLine("Enter birthday:");
+    string date = Console.ReadLine();
+
+    Console.WriteLine("Enter work number:");
+    string workNum = Console.ReadLine();
+
+    Console.WriteLine("Enter cell number:");
+    string cellNum = Console.ReadLine();
+    
+    Console.WriteLine(" Name: " + userName);
+    Console.WriteLine(" Birthdate: " + date);
+    Console.WriteLine(" Work: " + workNum);
+    Console.WriteLine(" Cell: " + cellNum);
   }
 }
